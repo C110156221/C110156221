f2d30a32016bacda825aa63f0adc75f0c516ac99
C110156221 <108121974+C110156221@users.noreply.github.com>
2022-06-30 07:43:07 +0800

    Update 題目70二進制的新編碼.py

--- 題目70二進制的新編碼.py
+++ 題目70二進制的新編碼.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 16 03:00:44 2021
-
-@author: ACER PREDATOR
-"""
 
 data=input().split(" ")
 cha="00 01 100 101 1100 1101 11100 11101 111100 111101".split(" ")
